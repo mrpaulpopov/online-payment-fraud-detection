215460e0d001f2840dab9b967e970bc57d4a620d
paul <paulpopovcv@gmail.com>
2026-09-06 23:40:00 +0500

    refactor: move hardcoded key from fastapi dependency to .env

--- src/app/dependencies.py
+++ src/app/dependencies.py
@@ -1,8 +1,12 @@
 from fastapi import Header, HTTPException
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os. environ.get("APP_API_KEY")
 
 def verify_api_key(x_api_key: str = Header(...)):
-    if x_api_key != "123":
+    if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API key")
 
     return x_api_key
