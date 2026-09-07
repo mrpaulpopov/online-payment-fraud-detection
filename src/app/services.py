c16dde344b1b9d5a954ddb84ab2ca47fd43b53e3
paul <paulpopovcv@gmail.com>
2026-09-07 14:18:44 +0500

    refactor(typing): add explicit type convertions for numpy arrays and booleans

--- src/app/services.py
+++ src/app/services.py
@@ -18,8 +18,8 @@ def graceful_degradation(transaction: dict) -> bool:
 
 
 def process_payment(transaction: dict, inference_meta: dict, model_lgbm: lgb.Booster) -> tuple[str, bool, float | None, str]:
-    fraud_probability = None
-    is_fraud = None
+    fraud_probability: float | None = None
+    is_fraud: bool = False
     business_decision, rule_reason = apply_business_rules(transaction)
 
     if business_decision is True:
@@ -27,19 +27,22 @@ def process_payment(transaction: dict, inference_meta: dict, model_lgbm: lgb.Boo
         return str(transaction["TransactionID"]), True, fraud_probability, reason
 
     try:
-        fraud_probability, is_fraud = inference_pipeline(transaction, inference_meta, model_lgbm)
+        raw_fraud_probability, raw_is_fraud = inference_pipeline(transaction, inference_meta, model_lgbm) # np.array
+        fraud_probability = float(raw_fraud_probability[0]) # np.array to float
+        is_fraud = bool(raw_is_fraud[0])
+
         if is_fraud:
-            reason = f"Fraud (Blocked by ML, probability: {(fraud_probability[0])*100:.1f}%)"
+            reason = f"Fraud (Blocked by ML, probability: {fraud_probability*100:.1f}%)"
         else:
             reason = "Legit (Passed ML)"
     except Exception as e: # noqa: BLE001
         logger.error(f"ML Pipeline failed: {e}. Falling back to Graceful Degradation.")
-        is_fraud = graceful_degradation(transaction)
+        is_fraud = bool(graceful_degradation(transaction))
         if is_fraud is True:
             reason = "Fraud (Blocked by Fallback rules)"
         else:
             reason = "Legit (Passed Fallback rules)"
 
     if fraud_probability is not None:
-        fraud_probability = round(fraud_probability[0], 4)
+        fraud_probability = round(fraud_probability, 4)
     return str(transaction["TransactionID"]), is_fraud, fraud_probability, reason
